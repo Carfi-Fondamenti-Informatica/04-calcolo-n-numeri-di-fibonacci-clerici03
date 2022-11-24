bd1f5037936743dafedd06a2efd3d8b6a435f363
clerici03 <115361946+clerici03@users.noreply.github.com>
2022-11-24 20:17:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,21 @@
 using namespace std;
 
 int main() {
+   int n=0;
+    int a=1, b=1, c=0;
+    cin>>n;
+    if (n<2) {
+        cout<< "errore"<<endl;
+    } else {
+        cout<< a <<endl;
+        cout<< b <<endl;
+        int i=0;
+        for (i=3; i<=n; i++){
+            c= a+b;
+            cout<< c <<endl;
+            a=b;
+            b=c;
+        }
+    }
    return 0;
 }
